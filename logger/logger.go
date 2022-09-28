f03a7bb477e0df3b9a202c802598f1fef1148cd2
pazevedobhbr <pr-azevedo@hotmail.com>
2022-09-28 08:55:53 -0300

    Initial commit

---
+++ logger/logger.go
@@ -0,0 +1,45 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// logger covers all the logs that can be registered by the application Banking and Banking Auth APIs
+// Paulo Azevedo
+
+var log *zap.Logger
+
+func init() {
+	var err error
+
+	config := zap.NewProductionConfig()
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.StacktraceKey = ""
+	config.EncoderConfig = encoderConfig
+
+	log, err = config.Build(zap.AddCallerSkip(1))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Info(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	log.Fatal(message, fields...)
+}
+
+func Debug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
+}
